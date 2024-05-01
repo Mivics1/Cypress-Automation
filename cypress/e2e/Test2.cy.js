5b62d1003716a18de0d8e69dc20872dd6e46db64
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-05-01 14:34:45 +0100

    CA-11 Added multi-checkbox promise

---
+++ cypress/e2e/Test2.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe("Test Suite for beginner",()=>{
+    it("Basic Assertions in Cypress",()=>{
+        //Visit the testing website
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        //Check checkbox and confirm the check promise 
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
+        //Handshake the previous promise
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        //Multi-check checkbox
+        cy.get('input[type="checkbox"]').check(['option2','option3'])
+    })
+})

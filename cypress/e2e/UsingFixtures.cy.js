35267de536af5b3f243707b9095f698a64e0f2d1
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-05-09 16:23:12 +0100

    CA-25 Updated the data driven test

--- cypress/e2e/UsingFixtures.cy.js
+++ cypress/e2e/UsingFixtures.cy.js
@@ -10,7 +10,8 @@ describe('Data driven test',function(){
     it('Using Fixtures attribute', function(){
         cy.visit('https://rahulshettyacademy.com/angularpractice/')
         cy.get('input[name="name"]:nth-child(2)').type(this.details.name)
+        cy.get('input[name="email"]').type(this.details.email) 
         cy.get('select').select(this.details.gender)
-        cy.get('input[name="email"]').type(this.details.email)
+
     })
 })

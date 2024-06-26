846a2e9bfb6d42c672342a70d144fa4cc746de90
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-06-26 13:14:00 +0100

    Update POMFixtures.cy.js

--- cypress/e2e/POMFixtures.cy.js
+++ cypress/e2e/POMFixtures.cy.js
@@ -1,6 +1,6 @@
 /// <reference types = "Cypress" />
-import HomePage from "./PageObjects/Homepage"
-import ProductPage from "./PageObjects/productPage"
+import HomePage from "./PageObjects/Homepage.js"
+import ProductPage from "./PageObjects/ProductPage.js"
 
 describe('Data driven test',function(){
     before(function(){

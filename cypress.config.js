bf663955422d7b58540d340fad219663226bd0b0
Agboola Michael Daramola <61335052+Mivics1@users.noreply.github.com>
2024-06-27 17:02:38 +0100

    Add awesome package to the project

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,14 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
+  projectId: '8n6iom',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   "defaultCommandTimeout" : 10000,
+  reporter: 'mochawesome',
   "env":{
     "url":"https://rahulshettyacademy.com"
   }
